1a45f69cc4fb694c2c842fff7bd7c4c6652e9969
VaninaClara <vaninamilov@gmail.com>
2023-04-21 19:53:01 +0200

    Correcciòn estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BreadListComponent } from './pages/bread-list/bread-list.component';
+import { CreateBreadComponent } from './pages/create-bread/create-bread.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 const routes: Routes = [
-  { path: 'bread-list', component: BreadListComponent }
+  { path: 'bread-list', component: BreadListComponent },
+  { path: 'create-bread', component: CreateBreadComponent },
+  { path: 'about-me', component: AboutMeComponent }
 ];
 
 @NgModule({
